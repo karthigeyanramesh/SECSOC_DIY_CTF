5173910ab817c7a2f4e5db0c851557fb0f0fcfde
karthigeyanramesh <117560582+karthigeyanramesh@users.noreply.github.com>
2024-06-24 14:10:52 +1000

    Update app.py - To print hello world to the root

--- app/app.py
+++ app/app.py
@@ -3,6 +3,14 @@
 
 # import the libraries...
 
+from flask import flask 
+
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0',port="5003")
 
